b79a698587f1688ab6ca5b2f769965670ffdea8b
arun43 <arun43@735d13b6-9817-0410-8766-e36946ffe9aa>
2013-04-08 08:02:46 +0000

    1.0 version updated
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/pits_rssnews/trunk@74358 735d13b6-9817-0410-8766-e36946ffe9aa

--- 1.0/pits_rssnews/Classes/Controller/PitsrssnewsController.php
+++ 1.0/pits_rssnews/Classes/Controller/PitsrssnewsController.php
@@ -28,7 +28,8 @@
  *
  *
  * @package pits_rssnews
- * @license http://www.gnu.org/licenses/gpl.html GNU General Public License, version 3 or later
+ * @license http://www.gnu.org/licenses/gpl.html GNU
+ * General Public License, version 3 or later
  *
  */
 
@@ -45,7 +46,8 @@ class Tx_PitsRssnews_Controller_PitsrssnewsController extends Tx_Extbase_MVC_Con
 	/**
 	 * injectPitsrssnewsRepository
 	 *
-	 * @param Tx_PitsRssnews_Domain_Repository_PitsrssnewsRepository $pitsrssnewsRepository
+	 * @param Tx_PitsRssnews_Domain_Repository_PitsrssnewsRepository
+	 * $pitsrssnewsRepository
 	 * @return void
 	 */
 	public function injectPitsrssnewsRepository(Tx_PitsRssnews_Domain_Repository_PitsrssnewsRepository $pitsrssnewsRepository) {
@@ -61,28 +63,41 @@ class Tx_PitsRssnews_Controller_PitsrssnewsController extends Tx_Extbase_MVC_Con
 		$pitsrssnewss = $this->pitsrssnewsRepository->findAll();
 		$this->view->assign('pitsrssnewss', $pitsrssnewss);
 		
-		//	Fetch flexform content
+	/**
+	 * Fetch flexform content
+	 */
+		
 		$array = $this->settings ['flexform'];
 		$invalidurl = 0;
-		//	Fetch contents from the external url
+		
+	/**
+	 * Fetch contents from the external url
+	 */	
+
 		$feedUrl = ($array['url']) ? $array['url'] : $this->settings['feedURL'];
 		if (!empty($feedUrl)) {
 		$rss_feed = file_get_contents($feedUrl);
 		$enc = mb_detect_encoding($rss_feed);
 		$data = mb_convert_encoding($rss_feed, 'UTF-8', $enc);
-		
-		//	Generate simple xml array from the fetched page content.
+
+	/**
+	 * Generate simple xml array from the fetched page content
+	 */	
+
 		$xml = new SimpleXmlElement($data, LIBXML_NOCDATA);
 		$xml_new = $this->simplexml2array($xml);
 
-		//	Passing the values to the template file
+	/**
+	 * Passing the values to the template file
+	 */		
+
 		$int_head = ($array['feedheader']) ? intval($array['feedheader']) : $this->settings['rssfeedHead'];
 		$this-> view-> assign('feedhead',$int_head);
 		$title = $xml_new['channel']['title'];
 		$this-> view-> assign('rss_head',$title);
 		$cnt_news = ($array['count']) ? intval($array['count']-1) : $this->settings['newsCount']-1;
 		$this-> view-> assign('cnt',$cnt_news);
-		$int_divider = ($array['contentdiv']) ? intval($array["contentdiv"]) : $this->settings['newsDivider'];
+		$int_divider = ($array['contentdiv']) ? intval($array['contentdiv']) : $this->settings['newsDivider'];
 		$this-> view-> assign('hbar',$int_divider);
 		$int_desc = ($array['crop_desc']) ? intval($array['crop_desc']) : $this->settings['cropDesc'];
 		$this-> view-> assign('desc',$int_desc);
@@ -95,28 +110,40 @@ class Tx_PitsRssnews_Controller_PitsrssnewsController extends Tx_Extbase_MVC_Con
 		$this-> view-> assign('xml_array',$xml_new['channel']['item']); 
 		} else {
 			$invalidurl = 1;
-			$locallangURL = Tx_Extbase_Utility_Localization::translate('invalid_url', $this->request->getControllerExtensionName(), $arguments=NULL);
+			$locallangURL = Tx_Extbase_Utility_Localization::translate('invalid_url',
+							$this->request->getControllerExtensionName(),
+							$arguments=NULL);
 			$this->flashMessageContainer->add($locallangURL);
 			$this-> view-> assign('validurl',$invalidurl);
 		}
 		
 	}
-	//	Function for parsing XML array
+	
+	/**
+	 * Function for parsing XML array
+	 */		
+
 	public function simplexml2array($xml) {
 		if (get_class($xml) == 'SimpleXMLElement') {
 			$attributes = $xml->attributes();
 			foreach($attributes as $k=>$v) {
-				if ($v) $a[$k] = (string) $v;
+				if ($v){
+					$a[$k] = (string) $v;
+				}
 			}
 			$x = $xml;
 			$xml = get_object_vars($xml);
 		}
 		if (is_array($xml)) {
-			if (count($xml) == 0) return (string) $x; // for CDATA
+			if (count($xml) == 0){
+				return (string) $x;
+			}
 			foreach($xml as $key=>$value) {
 				$r[$key] = $this->simplexml2array($value);
 			}
-			if (isset($a)) $r['@attributes'] = $a;    // Attributes
+			if (isset($a)){
+				$r['@attributes'] = $a;
+			}
 			return $r;
 		}
 		return (string) $xml;
